27ff03f7f90d3ad869687574b830cc99b364c25a
Hyunwoo Moon <mhw9163@gmail.com>
2021-01-14 14:32:57 -0800

    error

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -3,7 +3,8 @@
 function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-      response = { error: { message: 'server error' } }
+      response = { message: error.message, error }
+      // response = { error: { message: 'server error' } }
     } else {
       console.error(error)
       response = { message: error.message, error }
